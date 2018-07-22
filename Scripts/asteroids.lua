352cec349c8d095ab13559bf64981f8eb47194a9
Rat49 <rat49.xt@gmail.com>
2018-07-22 11:21:11 -0700

    moved asteroids on premake5

---
+++ Scripts/asteroids.lua
@@ -0,0 +1,103 @@
+workspace "Asteroids"
+	configurations { "Debug", "Release" }
+	platforms { "x64" }
+	location ( "../Generated/Projects" )
+	targetdir ( "../Generated/Bin" )
+	objdir ( "../Generated/Obj/%{prj.name}" )
+	
+	flags "FatalWarnings"
+		warnings "Extra"
+        
+	filter "configurations:Debug"
+		defines { "_DEBUG" }
+		symbols "On"
+		optimize "Off"
+		targetsuffix "_d"
+	
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		symbols "Off"
+		optimize "On"
+		
+	filter "platforms:x64"
+		architecture "x64"
+		system "Windows"
+	
+	filter {}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+	project "Asteroids"
+		kind "WindowedApp"
+		language "C++"
+		characterset "MBCS"
+		defines { "SFML_STATIC" }
+		
+		local SourceDir = "../Source";
+
+		files
+		{
+			SourceDir .. "/**.h",
+			SourceDir .. "/**.cpp",
+		}
+	   
+		vpaths
+		{
+			["Engine/*"] = 
+		    {
+				SourceDir .. "/Engine/**.cpp",
+				SourceDir .. "/Engine/**.h",
+			},
+			["Game/*"] = 
+			{
+				SourceDir .. "/Game/**.cpp",
+				SourceDir .. "/Game**.h",
+			},
+		}
+		
+		postbuildcommands
+		{
+			"{COPY} '$(SFML_ROOT)/bin/openal32.dll' '%{cfg.targetdir}'"
+		}
+		
+		includedirs
+		{
+			"$(SFML_ROOT)/include"
+		}
+		
+		libdirs
+		{
+			"$(SFML_ROOT)/lib"
+		}
+		
+		
+		links
+		{
+			"opengl32.lib",
+			"freetype.lib",
+			"jpeg.lib",
+			"ws2_32.lib",
+			"winmm.lib",
+			"openal32.lib",
+			"gdi32.lib",
+		}
+		
+		filter { "configurations:Debug" }
+			links
+			{
+				"sfml-window-s-d.lib",
+				"sfml-system-s-d.lib",
+				"sfml-graphics-s-d.lib",
+				"sfml-audio-s-d.lib",
+				"sfml-network-s-d.lib"
+			}
+			
+		filter { "configurations:Release" }
+			links
+			{
+				"sfml-window-s.lib",
+				"sfml-system-s.lib",
+				"sfml-graphics-s.lib",
+				"sfml-audio-s.lib",
+				"sfml-network-s.lib"
+			}
+-----------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ generate.bat
@@ -0,0 +1,2 @@
+cd Scripts
+premake5.exe vs2017 --file=asteroids.lua
